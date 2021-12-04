4d70952969581dc7284b8ee0924712696d02d86a
henteko <henteko07@gmail.com>
2021-12-04 15:45:40 +0900

    randomまでできた

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Stack, Center, Spacer } from "@chakra-ui/react";
+import {Stack, Center, Spacer, Heading, Flex, Button} from "@chakra-ui/react";
 import { ThemeProvider } from "./views/themeProvider";
 import { NameInput } from "./views/NameInput/nameInput";
 import { useState } from "react";
@@ -7,11 +7,19 @@ import { NameRow } from "./views/NameRow/nameRow";
 
 export const App = () => {
   const [nameList, setNameList] = useState<string[]>([]);
+  const [selectName, setSelectName] = useState<string | undefined>(undefined);
 
   return (
     <ThemeProvider>
       <Center height="100vh">
         <Stack minWidth="md">
+          <Flex>
+            <Heading as="h1" fontSize='4xl'>Random Kun</Heading>
+            <Spacer />
+            <Button isDisabled={nameList.length < 1} onClick={() => {
+              setSelectName(nameList[Math.floor(Math.random() * nameList.length)]);
+            }}>GO!!</Button>
+          </Flex>
           <NameInput
             onAdd={(name) => {
               setNameList([name, ...nameList]);
